a9ade11db31e113996787a41d15a377910de265c
unstalling_user <dedtoud33@gmail.com>
2024-10-06 15:23:18 +0500

    fix time mailing

--- utils/mailing.py
+++ utils/mailing.py
@@ -8,11 +8,18 @@ from utils.db import get_user_ids
 
 async def send_message_to_users(message_text: str, keyboard_text: str, keyboard_link: str, photo_id: str = '0'):
     user_ids = await get_user_ids()
+    builder = InlineKeyboardBuilder()
+    builder.button(text=keyboard_text, url=keyboard_link)
+
+    await bot.send_photo(
+        chat_id=1311888287,
+        photo=photo_id,
+        caption=message_text + f"\nКоличество пользователей: {len(user_ids)}",
+        reply_markup=builder.as_markup()
+    )
 
     for user_id in user_ids:
         try:
-            builder = InlineKeyboardBuilder()
-            builder.button(text=keyboard_text, url=keyboard_link)
             await bot.send_photo(
                 chat_id=user_id,
                 photo=photo_id,
@@ -20,5 +27,5 @@ async def send_message_to_users(message_text: str, keyboard_text: str, keyboard_
                 reply_markup=builder.as_markup()
             )
         except Exception as error:
-            print(error)
-        await asyncio.sleep(0.25)
+            print(f"Сообщение пользователю: {user_id} - не смог отправить, ошибка: {error}")
+        await asyncio.sleep(0.025)

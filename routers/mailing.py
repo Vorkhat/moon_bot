4328cab06949da80f7de6d5bb5003aca8e04129e
unstalling_user <dedtoud33@gmail.com>
2024-10-06 19:58:35 +0500

    fix mailing

--- routers/mailing.py
+++ routers/mailing.py
@@ -1,5 +1,5 @@
+import asyncio
 import re
-
 from aiogram import Router, F, types
 from aiogram.types import Message, CallbackQuery, InputFile
 from aiogram.utils.keyboard import InlineKeyboardBuilder
@@ -12,14 +12,12 @@ from utils.mailing import send_message_to_users
 
 create_mailing = Router()
 
-
 class MailingStates(StatesGroup):
     waiting_for_text = State()
     waiting_for_button_text = State()
     waiting_for_keyboard_link = State()
-    waiting_for_image = State()  # Новый шаг для выбора изображения
+    waiting_for_image = State()
     confirm = State()
-
 
 @create_mailing.message(F.text.lower() == "создать рассылку")
 async def create_mailing_router(message: Message, state: FSMContext):
@@ -31,7 +29,6 @@ async def create_mailing_router(message: Message, state: FSMContext):
         reply_markup=builder.as_markup()
     )
     await state.set_state(MailingStates.waiting_for_text)
-
 
 @create_mailing.message(MailingStates.waiting_for_text)
 async def handle_mailing_text(message: Message, state: FSMContext):
@@ -46,7 +43,6 @@ async def handle_mailing_text(message: Message, state: FSMContext):
     )
     await state.set_state(MailingStates.waiting_for_button_text)
 
-
 @create_mailing.message(MailingStates.waiting_for_button_text)
 async def handle_button_text(message: Message, state: FSMContext):
     builder = InlineKeyboardBuilder()
@@ -59,13 +55,13 @@ async def handle_button_text(message: Message, state: FSMContext):
     )
     await state.set_state(MailingStates.waiting_for_keyboard_link)
 
-
 @create_mailing.message(MailingStates.waiting_for_keyboard_link)
 async def handle_keyboard_link(message: Message, state: FSMContext):
     if re.match(r"^https://t\.me/[A-Za-z0-9_]+/app\?startapp$", message.text):
         await state.update_data(keyboard_link=message.text)
 
         builder = InlineKeyboardBuilder()
+        builder.button(text="Пропустить фото", callback_data="skip_photo")
         builder.button(text="Назад", callback_data="back_to_button_text")
 
         await message.answer(
@@ -75,7 +71,6 @@ async def handle_keyboard_link(message: Message, state: FSMContext):
         await state.set_state(MailingStates.waiting_for_image)
     else:
         await message.answer(text="Ссылка указана неверно")
-
 
 @create_mailing.message(MailingStates.waiting_for_image, F.photo)
 async def handle_image(message: Message, state: FSMContext):
@@ -109,22 +104,41 @@ async def handle_image(message: Message, state: FSMContext):
     )
     await state.set_state(MailingStates.confirm)
 
-
 @create_mailing.callback_query(F.data)
 async def handle_callbacks(callback: CallbackQuery, state: FSMContext):
     data = await state.get_data()
     mailing_text = data.get('mailing_text', '')
     button_text = data.get('button_text', '')
     keyboard_link = data.get('keyboard_link', '')
-    photo = data.get('photo', '')
+    photo = data.get('photo', None)
 
     if callback.data == "confirm":
         await callback.message.answer("Рассылка успешно начата")
-        if photo:
-            await send_message_to_users(mailing_text, button_text, keyboard_link, photo)
-        else:
-            await send_message_to_users(mailing_text, button_text, keyboard_link)
+        await asyncio.create_task(send_message_to_users(mailing_text, button_text, keyboard_link, photo))
         await state.clear()
+
+    elif callback.data == "skip_photo":
+        await state.update_data(photo=None)
+
+        buttons = [
+            [
+                types.InlineKeyboardButton(
+                    text=button_text,
+                    url=keyboard_link
+                )
+            ],
+            [
+                types.InlineKeyboardButton(text="Подтвердить", callback_data="confirm"),
+                types.InlineKeyboardButton(text="Назад к ссылке", callback_data="back_to_link")
+            ],
+        ]
+        keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
+
+        await callback.message.answer(
+            text=f'Проверь текст рассылки и нажми "Подтвердить", чтобы запустить:\n\n{mailing_text}',
+            reply_markup=keyboard
+        )
+        await state.set_state(MailingStates.confirm)
 
     elif callback.data == "back_to_text":
         await state.set_state(MailingStates.waiting_for_text)
--- utils/mailing.py
+++ utils/mailing.py
@@ -1,31 +1,47 @@
 import asyncio
-
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-
+from prisma import Prisma
 from config_reader import bot
-from utils.db import get_user_ids
+
+
+async def get_user_ids(prisma: Prisma, offset: int = 0, limit: int = 100):
+    users = await prisma.user.find_many(
+        skip=offset,
+        take=limit,
+        where={"access": True}
+    )
+    return [user.tgUserId for user in users]
 
 
 async def send_message_to_users(message_text: str, keyboard_text: str, keyboard_link: str, photo_id: str = '0'):
-    user_ids = await get_user_ids()
+    offset = 0
+    limit = 100
     builder = InlineKeyboardBuilder()
     builder.button(text=keyboard_text, url=keyboard_link)
 
-    await bot.send_photo(
-        chat_id=1311888287,
-        photo=photo_id,
-        caption=message_text + f"\nКоличество пользователей: {len(user_ids)}",
-        reply_markup=builder.as_markup()
-    )
+    async with Prisma() as prisma:
+        while True:
+            user_ids = await get_user_ids(prisma, offset=offset, limit=limit)
 
-    for user_id in user_ids:
-        try:
-            await bot.send_photo(
-                chat_id=user_id,
-                photo=photo_id,
-                caption=message_text,
-                reply_markup=builder.as_markup()
-            )
-        except Exception as error:
-            print(f"Сообщение пользователю: {user_id} - не смог отправить, ошибка: {error}")
-        await asyncio.sleep(0.025)
+            if not user_ids:
+                break
+
+            for user_id in user_ids:
+                try:
+                    await bot.send_photo(
+                        chat_id=user_id,
+                        photo=photo_id,
+                        caption=message_text,
+                        reply_markup=builder.as_markup()
+                    )
+                except Exception as error:
+                    print(f"Сообщение пользователю: {user_id} - не смог отправить, ошибка: {error}")
+
+                    await prisma.user.update(
+                        where={"tgUserId": user_id},
+                        data={"access": False}
+                    )
+
+                await asyncio.sleep(0.025)
+
+            offset += limit

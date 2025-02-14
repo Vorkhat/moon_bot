bd07dd3fd61acd4e6b37b3f540a0814785c30e0d
unstalling_user <dedtoud33@gmail.com>
2025-02-14 17:18:57 +0500

    ,

--- utils/mailing.py
+++ utils/mailing.py
@@ -1,11 +1,7 @@
 import asyncio
-import logging
-
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from prisma import Prisma
 from config_reader import bot
-
-MAX_MESSAGES_PER_SECOND = 25
 
 
 async def get_user_ids(prisma: Prisma, offset: int = 0, limit: int = 100):
@@ -17,40 +13,35 @@ async def get_user_ids(prisma: Prisma, offset: int = 0, limit: int = 100):
     return [user.tgUserId for user in users]
 
 
-async def send_message(user_id, message_text, keyboard, photo_id, prisma, semaphore):
-    async with semaphore:
-        try:
-            await bot.send_photo(
-                chat_id=user_id,
-                photo=photo_id,
-                caption=message_text,
-                reply_markup=keyboard
-            )
-            logging.info('Message sent to user %s', user_id)
-        except Exception:
-            await prisma.user.update(
-                where={"tgUserId": user_id},
-                data={"access": False}
-            )
-
-
 async def send_message_to_users(message_text: str, keyboard_text: str, keyboard_link: str, photo_id: str = '0'):
     offset = 0
     limit = 100
-    semaphore = asyncio.Semaphore(MAX_MESSAGES_PER_SECOND)
-
     builder = InlineKeyboardBuilder()
     builder.button(text=keyboard_text, url=keyboard_link)
-    keyboard = builder.as_markup()
 
     async with Prisma() as prisma:
         while True:
             user_ids = await get_user_ids(prisma, offset=offset, limit=limit)
+
             if not user_ids:
                 break
 
-            tasks = [send_message(user_id, message_text, keyboard, photo_id, prisma, semaphore) for user_id in user_ids]
-            await asyncio.gather(*tasks)
+            for user_id in user_ids:
+                try:
+                    await bot.send_photo(
+                        chat_id=user_id,
+                        photo=photo_id,
+                        caption=message_text,
+                        reply_markup=builder.as_markup()
+                    )
+                except Exception as error:
+                    print(f"Сообщение пользователю: {user_id} - не смог отправить, ошибка: {error}")
+
+                    await prisma.user.update(
+                        where={"tgUserId": user_id},
+                        data={"access": False}
+                    )
+
+                await asyncio.sleep(0.01)
 
             offset += limit
-            await asyncio.sleep(1)
